0da99ee0c7202233bda0095c7432bc7f4a17da94
Takashiro <takashiro@qq.com>
2017-11-05 11:54:33 +0800

    Connect to local host via default port automatically

--- js/main.js
+++ js/main.js
@@ -34,8 +34,17 @@ server.on('close', ()=>{
 });
 
 $(()=>{
-	if ($_GET['server']) {
-		showMessage('Loading...');
+	showMessage('Loading...');
+	if($_GET['server']){
 		server.connect($_GET['server']);
+	}else{
+		let match = location.href.match(/(\w+)\:\/\/(.*?)(?:\/.*)?/i);
+		if(match){
+			if(match[1] == 'file'){
+				server.connect('localhost');
+			}else{
+				serrver.connect(match[2]);
+			}
+		}
 	}
 });
