9928218579fe9824188b52d36a58f8baffcf6f25
Takashiro <takashiro@qq.com>
2017-11-20 23:59:15 +0800

    Fix a bug of regexp that host name can't be parsed

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ $(()=>{
 	if($_GET['server']){
 		server.connect($_GET['server']);
 	}else{
-		let match = location.href.match(/(\w+)\:\/\/(.*?)(?:\/.*)?/i);
+		let match = location.href.match(/^(\w+)\:\/\/(.*?)(?:\/.*)?$/i);
 		if(match){
 			if(match[1] == 'file'){
 				server.connect('localhost');
